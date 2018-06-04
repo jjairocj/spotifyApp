010ef7e7ed0f86e9330f9f82c1f4ec2d7c42b741
Jairo Cruz <jhon_jairo@live.com>
2018-06-04 16:01:54 -0500

    http Get

--- spotifyApp/src/app/components/home/home.component.ts
+++ spotifyApp/src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-home',
@@ -7,7 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  paises: any[] = [];
+  constructor( private http: HttpClient ) {
+    this.http.get('https://restcountries.eu/rest/v2/lang/es').subscribe( (resp: any) => {
+      this.paises = resp;
+      console.log(resp);
+    } );
+  }
 
   ngOnInit() {
   }
